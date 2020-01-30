5912248d135c1e06570dfafbe0948b09174709cb
Giovani Aguirre <me@its-g.io>
2020-01-30 15:44:49 -0600

    Edit now available for people.

--- server/controllers/people.js
+++ server/controllers/people.js
@@ -7,9 +7,9 @@ module.exports.getPeople = (req, res) => res.status(200).json(people);
 
 module.exports.postPerson = (req, res) => {
   const person = req.body;
-  people[dataLength-1].id === id ? id += 1 : id;
+  people[dataLength-1].id === id ? id++ : id;
   people.push({id, ...person});
-  // id += 1;
+  id++;
 
   res.status(200).send(people);
 }
@@ -20,5 +20,25 @@ module.exports.deletePerson = (req, res) => {
   if (targetIndex === -1) return res.status(404).send(`${id} does not exist!`);
   people.splice(targetIndex, 1);
 
+  res.status(200).json(people);
+}
+
+module.exports.editPerson = (req, res) => {
+  let { id } = req.params;
+  let { first_name, last_name, c_in, c_out, meal, job, available, skills, decessed } = req.body;
+  let targetIndex = people.findIndex(person => person.id === +id);
+  if (targetIndex === -1) return res.status(404).send(`${id} does not exist!`)
+  let foundPerson = people[targetIndex];
+
+  foundPerson.first_name = first_name;
+  foundPerson.last_name = last_name;
+  foundPerson.c_in = c_in;
+  foundPerson.c_out = c_out;
+  foundPerson.meal = meal;
+  foundPerson.job = job;
+  foundPerson.available = available;
+  foundPerson.skills = skills;
+  foundPerson.decessed = decessed;
+  
   res.status(200).json(people);
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 2020;
-const { getPeople, postPerson, deletePerson } = require("./controllers/people");
+const { getPeople, postPerson, deletePerson, editPerson } = require("./controllers/people");
 
 app
   .use(express.json())
   .get("/api/people", getPeople)
   .post("/api/people", postPerson)
   .delete("/api/people/:id", deletePerson)
+  .put("/api/people/:id", editPerson)
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
