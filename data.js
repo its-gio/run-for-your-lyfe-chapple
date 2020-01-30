ee1c8c9259e60eeade393c2fc3d8873d29b8281e
Giovani Aguirre <me@its-g.io>
2020-01-30 13:55:21 -0600

    Added 5 people to 'database'

---
+++ data.js
@@ -0,0 +1,80 @@
+module.exports.data = [
+  { id: 0,
+    first_name: "Gio",
+    last_name: "Aguirre",
+    c_in: "",
+    c_out: "",
+    meal: [
+      { breakfast: false, lunch: false, dinner: false, date: "" }
+    ],
+    job: "Leader",
+    available: false,
+    skills: { orginization: 5, farming: 2, scavanging: 4 },
+    decessed: false
+  },
+  { id: 1,
+    first_name: "Joe",
+    last_name: "Shmoes",
+    c_in: "",
+    c_out: "",
+    meal: [
+      { breakfast: false, lunch: false, dinner: false, date: "" }
+    ],
+    job: "Farmer",
+    available: false,
+    skills: { orginization: 1, farming: 4, scavanging: 2 },
+    decessed: false
+  },
+  { id: 2,
+    first_name: "Sally",
+    last_name: "Shmoes",
+    c_in: "",
+    c_out: "",
+    meal: [
+      { breakfast: false, lunch: false, dinner: true, date: "" }
+    ],
+    job: "Scavenger",
+    available: false,
+    skills: { orginization: 1, farming: 3, scavanging: 4 },
+    decessed: false
+  },
+  { id: 3,
+    first_name: "Guy",
+    last_name: "Guyson",
+    c_in: "",
+    c_out: "",
+    meal: [
+      { breakfast: true, lunch: false, dinner: true, date: "" }
+    ],
+    job: "Guard",
+    available: false,
+    skills: { guard: 5 },
+    decessed: false
+  },
+  { id: 4,
+    first_name: "Billy",
+    last_name: "Guyson",
+    c_in: "",
+    c_out: "",
+    meal: [
+      { breakfast: true, lunch: false, dinner: true, date: "" }
+    ],
+    job: "",
+    available: true,
+    skills: { cooking: 3 },
+    decessed: false
+  },
+  { id: 5,
+    first_name: "Niel",
+    last_name: "dedman",
+    c_in: "",
+    c_out: "",
+    meal: [
+      { breakfast: false, lunch: false, dinner: false, date: "" }
+    ],
+    job: "",
+    available: true,
+    skills: {},
+    decessed: true
+  }
+]
