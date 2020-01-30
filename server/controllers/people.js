fddfc0707d94598c1a9d6a09135c93adae8f3d21
Giovani Aguirre <me@its-g.io>
2020-01-30 15:25:10 -0600

    Delete endpoint available.

--- server/controllers/people.js
+++ server/controllers/people.js
@@ -5,11 +5,20 @@ let id = dataLength;
 
 module.exports.getPeople = (req, res) => res.status(200).json(people);
 
-module.exports.postPeople = (req, res) => {
+module.exports.postPerson = (req, res) => {
   const person = req.body;
   people[dataLength-1].id === id ? id += 1 : id;
   people.push({id, ...person});
-  id += 1;
+  // id += 1;
 
   res.status(200).send(people);
+}
+
+module.exports.deletePerson = (req, res) => {
+  let { id } = req.params;
+  let targetIndex = people.findIndex(person => person.id === +id)
+  if (targetIndex === -1) return res.status(404).send(`${id} does not exist!`);
+  people.splice(targetIndex, 1);
+
+  res.status(200).json(people);
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 2020;
-const { getPeople, postPeople } = require("./controllers/people");
+const { getPeople, postPerson, deletePerson } = require("./controllers/people");
 
 app
   .use(express.json())
   .get("/api/people", getPeople)
-  .post("/api/people", postPeople)
+  .post("/api/people", postPerson)
+  .delete("/api/people/:id", deletePerson)
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
