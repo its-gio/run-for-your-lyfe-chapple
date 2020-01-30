c4cabee1aa0e38c7c168ef35b794c429c18d4bc1
Giovani Aguirre <me@its-g.io>
2020-01-30 15:09:58 -0600

    c_in and c_out now have dates connected for data.js

--- data.js
+++ data.js
@@ -2,10 +2,10 @@ module.exports.data = [
   { id: 0,
     first_name: "Gio",
     last_name: "Aguirre",
-    c_in: "",
-    c_out: "",
+    c_in: "1/30/2020",
+    c_out: null,
     meal: [
-      { breakfast: false, lunch: false, dinner: false, date: "" }
+      { breakfast: false, lunch: false, dinner: false, date: "1/30/2020" }
     ],
     job: "Leader",
     available: false,
@@ -15,10 +15,10 @@ module.exports.data = [
   { id: 1,
     first_name: "Joe",
     last_name: "Shmoes",
-    c_in: "",
-    c_out: "",
+    c_in: "1/30/2020",
+    c_out: null,
     meal: [
-      { breakfast: false, lunch: false, dinner: false, date: "" }
+      { breakfast: false, lunch: false, dinner: false, date: "1/30/2020" }
     ],
     job: "Farmer",
     available: false,
@@ -28,10 +28,10 @@ module.exports.data = [
   { id: 2,
     first_name: "Sally",
     last_name: "Shmoes",
-    c_in: "",
-    c_out: "",
+    c_in: "1/30/2020",
+    c_out: null,
     meal: [
-      { breakfast: false, lunch: false, dinner: true, date: "" }
+      { breakfast: false, lunch: false, dinner: true, date: "1/30/2020" }
     ],
     job: "Scavenger",
     available: false,
@@ -41,10 +41,10 @@ module.exports.data = [
   { id: 3,
     first_name: "Guy",
     last_name: "Guyson",
-    c_in: "",
-    c_out: "",
+    c_in: "1/30/2020",
+    c_out: null,
     meal: [
-      { breakfast: true, lunch: false, dinner: true, date: "" }
+      { breakfast: true, lunch: false, dinner: true, date: "1/30/2020" }
     ],
     job: "Guard",
     available: false,
@@ -54,10 +54,10 @@ module.exports.data = [
   { id: 4,
     first_name: "Billy",
     last_name: "Guyson",
-    c_in: "",
-    c_out: "",
+    c_in: "1/30/2020",
+    c_out: null,
     meal: [
-      { breakfast: true, lunch: false, dinner: true, date: "" }
+      { breakfast: true, lunch: false, dinner: true, date: "1/30/2020" }
     ],
     job: "",
     available: true,
@@ -67,10 +67,10 @@ module.exports.data = [
   { id: 5,
     first_name: "Niel",
     last_name: "dedman",
-    c_in: "",
-    c_out: "",
+    c_in: "1/30/2020",
+    c_out: "1/30/2020",
     meal: [
-      { breakfast: false, lunch: false, dinner: false, date: "" }
+      { breakfast: false, lunch: false, dinner: false, date: "1/30/2020" }
     ],
     job: "",
     available: true,
