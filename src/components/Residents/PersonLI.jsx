4ca33980f8784307d0f8779105381d7e949eb0f0
Giovani Aguirre <me@its-g.io>
2020-01-30 16:58:50 -0600

    Styled residents list lightly

--- src/components/Residents/PersonLI.jsx
+++ src/components/Residents/PersonLI.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 export default function PersonLI(props) {
 
   return (
-    <li>
-      {props.person.first_name}
-    </li>
+    <>
+      <li>
+        {props.person.first_name}
+      </li>
+      <hr/>
+    </>
   )
 }
