c7799eb13784c53018e977a3e3c91ea404777c58
Giovani Aguirre <me@its-g.io>
2020-01-31 11:40:35 -0600

    Testing out modal idea

--- src/components/Residents/PersonLI.jsx
+++ src/components/Residents/PersonLI.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React from 'react';
+import PersonLI_D from "./PersonLI_D";
+import PersonLI_E from "./PersonLI_E";
 
-export default function PersonLI(props) {
-  const { first_name, last_name, c_in, c_out, job, meal, available, decessed } = props.person;
-  return (
-    <>
-      <li>
-        <span>{first_name}</span>
-        <span>{last_name}</span>
-        <span>{c_in}</span>
-        <span>{c_out ? c_out : "None" }</span>
-        <span>meals</span>
-        <span>{job}</span>
-        <span>{available}</span>
-        <span>Learn More ></span>
-        <span><input checked={ decessed ? "check" : "" } type="checkbox"/></span>
-      </li>
-      <hr/>
-    </>
-  )
+export default class PersonLI extends React.Component{
+  constructor() {
+    super();
+
+    this.state = {
+      person: {},
+      edit: false
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ person: {...this.props.person} })
+  }
+
+  editChange = (e) => {
+    this.setState({ edit: !this.state.edit })
+  }
+
+  sendEdits = () => {
+    this.editChange()
+  }
+
+  render () {
+    return <PersonLI_D editChange={this.editChange} person={this.state.person} />
+  }
 }
---
+++ src/components/Residents/PersonLI_D.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function PersonLI_D(props) {
+  const { first_name, last_name, c_in, c_out, job, available, decessed } = props.person;
+  let availableColor = available ? "green" : "red";
+
+  return (
+    <>
+      <li onDoubleClick={props.editChange}>
+        <span>{first_name}</span>
+        <span>{last_name}</span>
+        <span>{c_in}</span>
+        <span>{c_out ? c_out : "None" }</span>
+        <span>meals</span>
+        <span>{job}</span>
+        <span><span style={{backgroundColor: availableColor}} className="available"></span></span>
+        <span>Learn More ></span>
+        <span><input checked={ decessed ? "check" : "" } type="checkbox"/></span>
+      </li>
+      <hr/>
+    </>
+  )
+}
