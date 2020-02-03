c5e6c2d2b99a3819d4768ab7e711d4067d8d8192
Giovani Aguirre <me@its-g.io>
2020-02-03 15:10:52 -0600

    Adding homepage image grid

---
+++ src/components/defaults/Images.jsx
@@ -0,0 +1,8 @@
+import BangalowCloseup from "../../img/Bangalow-closeup.jpg";
+import BangalowCloseup2 from "../../img/Bangalow-closeup-2.jpg";
+import BeachShot from "../../img/Beach-shot.jpg";
+import Outdoor from "../../img/Outdoor.jpg";
+import PoolArea from "../../img/Pool-area.jpg";
+import ResortOverview from "../../img/Resort-Overview.jpg";
+
+export { BangalowCloseup, BangalowCloseup2, BeachShot, Outdoor, PoolArea, ResortOverview };
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import Header from "../defaults/Header"
+import { BangalowCloseup, BangalowCloseup2, BeachShot, Outdoor, PoolArea, ResortOverview } from "../defaults/Images";
 
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <Header title="Run For Your Lyfe Chapple" />
-        <button onClick={() => this.props.changePage("residents")}>See Survivors</button>
+      <div className="home">
+        <div className="home--loggin">
+          <Header title="Run For Your Lyfe Chapple" />
+          <button onClick={() => this.props.changePage("residents")}>See Survivors</button>
+        </div>
+        <div className="image-grid">
+          <img src={Outdoor} alt=""/>
+          <img src={ResortOverview} alt=""/>
+          <img src={BangalowCloseup} alt=""/>
+          <img src={PoolArea} alt=""/>
+          <img src={BangalowCloseup2} alt=""/>
+          <img src={BeachShot} alt=""/>
+        </div>
       </div>
     )
   }
