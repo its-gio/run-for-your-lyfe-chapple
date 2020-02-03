209725c7c5418a8edfb6aed43c1093584e030d3c
Giovani Aguirre <me@its-g.io>
2020-02-03 10:51:54 -0600

    Connected meals to colors in PersonLiDiaply.js

--- src/components/Residents/PersonLiDisplay.jsx
+++ src/components/Residents/PersonLiDisplay.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
 export default function PersonLiDisplay(props) {
-  const { first_name, last_name, c_in, c_out, job, available } = props.person;
-  let availableColor = available ? "green" : "red";
+  const { first_name, last_name, c_in, c_out, job } = props.person;
+
+  let circleColor = (key) => {
+    if (key === "available") return props.person[key] ? "green" : "red";
+
+    return props.person.meal[0][key] ? "green" : "red";
+  }
 
   return (
     <>
@@ -11,9 +16,13 @@ export default function PersonLiDisplay(props) {
         <span>{last_name}</span>
         <span>{c_in}</span>
         <span>{c_out ? c_out : "None" }</span>
-        <span className="meals"><span className="meal">B: <span className="meal--eaten"></span> </span> <span className="meal">L: <span className="meal--eaten"></span> </span> <span className="meal">D: <span className="meal--eaten"></span> </span> </span>
+        <span className="meals">
+          <span className="meal">B: <span style={{backgroundColor: circleColor("breakfast")}} className="meal--eaten"></span></span>
+          <span className="meal">L: <span style={{backgroundColor: circleColor("lunch")}} className="meal--eaten"></span></span>
+          <span className="meal">D: <span style={{backgroundColor: circleColor("dinner")}} className="meal--eaten"></span></span>
+        </span>
         <span>{job}</span>
-        <span><span style={{backgroundColor: availableColor}} className="available"></span></span>
+        <span><span style={{backgroundColor: circleColor("available")}} className="available"></span></span>
         <span>Learn More ></span>
       </li>
       <hr/>
